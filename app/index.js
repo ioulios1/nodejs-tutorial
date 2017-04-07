2ff8c0d80edc8a0043454b0d836e537c8caec91b
ioulios ciko <ioulios@skymakers.biz>
2017-04-07 11:42:31 +0300

    στήσιμο engine για το layout.hbs

--- app/index.js
+++ app/index.js
@@ -5,8 +5,11 @@ const passport = require('passport')
 const session = require('express-session')
 const pg = require('pg')
 const pgSession = require('connect-pg-simple')(session)
+const exphbs = require('express-handlebars')
+const path = require('path')
 
 const app = express()
+
 app.use(session({
   strore : new (require('connect-pg-simple')(session))(),
   sexret : process.env.FOO_COOKIE_SECRET,
@@ -16,5 +19,16 @@ app.use(session({
 
 app.use(passport.initialize())
 app.use(passport.session())
+
+app.engine('.hbs',exphbs({
+  defaultLayout : 'layout',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname)
+}))
+
+app.set('view engine','.hbs')
+app.set('views',__dirname)
+
+
 
 module.exports = app
