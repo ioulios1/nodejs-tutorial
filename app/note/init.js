238a1cb0b23c505e0ecb44b2145d04fd77222378
ioulios ciko <ioulios@skymakers.biz>
2017-04-12 13:17:46 +0300

    adding get for calling note handler

--- app/note/init.js
+++ app/note/init.js
@@ -1,14 +1,19 @@
 const passport = require('passport')
+const noteHandler=require('./noteHandler')
 
 function init(app){
-    //app.get('/notes',passport.authenticationMiddleware(),renderNote)
-    app.get('/notes',renderNote)
+    app.get('/notes',passport.authenticationMiddleware(),renderNote)
+
+    app.get('/setNote',passport.authenticationMiddleware(),noteHandler.setNote)
+
 }
 
 
 function renderNote(req,res)
 {
-  res.render('note/note')
+  //noteHandler.getNotes
+  //console.log('render note '+req.user.notes)
+  res.render('note/note',req.user.notes)
 }
 
 module.exports.init=init
